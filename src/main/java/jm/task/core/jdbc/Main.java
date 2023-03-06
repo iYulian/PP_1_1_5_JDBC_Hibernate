a4882087323aa1ce9dff987e939ab2f4c47b759c
iYulian <yulianlemur@gmail.com>
2023-03-06 14:41:51 +0300

    First commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Petrov", (byte) 120);
+        userService.saveUser("Jilly", "Ivanov", (byte) 54);
+        userService.saveUser("Nose", "Jose", (byte) 36);
+        userService.saveUser("Klement", "Kally", (byte) 18);
+
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
